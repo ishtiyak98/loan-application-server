c1a37cdce2ee2f7112e1966cdb97cfa660044eb1
ishtiyak98 <rifatishtiyak@gmail.com>
2022-06-08 16:30:31 +0600

    server added

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.c0cq6.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const userCollection = client.db("loanApp").collection("userDetails");
+
+    app.post("/user-details", async(req, res)=>{
+        const newUser = req.body;
+        const result = await userCollection.insertOne(newUser);
+        res.send(result);
+    })
+    
+  } 
+  finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server");
+});
+
+app.listen(port, () => {
+  console.log("listening from port: ", port);
+});
